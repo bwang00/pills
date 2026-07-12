8543b7d825699272b19bcfd06471925f577c7f9f
bwang00 <ben00wang@gmail.com>
2026-07-12 15:10:19 +0800

    fix: use svh for better Chrome mobile support

--- frontend/src/pages/AIChatPage.tsx
+++ frontend/src/pages/AIChatPage.tsx
@@ -144,7 +144,7 @@ export default function AIChatPage() {
 
   return (
     <Layout title="聊天">
-      <div className="flex h-[calc(100dvh-80px)] relative">
+      <div className="flex flex-col" style={{ height: 'calc(100svh - 80px)' }}>
         {/* Main Chat Area */}
         <div className="flex-1 flex flex-col min-w-0">
           {/* Mobile: sidebar toggle button */}
